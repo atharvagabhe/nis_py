6b6a9886e741d960149ac6b723ae1a7c4213d54f
Ubuntu <atharvagabhe89@gmail.com>
2018-03-21 23:31:02 +0100

    path adjusted

--- nis.py
+++ nis.py
@@ -1,10 +1,14 @@
 ###########import###############
 import json
 import requests
-
+import time
 ###########global###############
 read_count=0
+sleep_time=2
 
+
+
+##########functions#############
 def getTemperature():
 	url = 'http://192.168.178.108:8080/Thingworx/Things/nectThing/Properties/Temperature'
 	headers = {'appKey': 'c56e4f1b-d1d4-4f9d-ab20-0bf3c08dc363','Accept': 'application/json'}
@@ -13,9 +17,17 @@ def getTemperature():
 	print(s)
 	return s
 
-while(read_count<100):
-        x=getTemperature()
-        f= open('readings.csv','a')
-        f.write(str(x))
-        f.close()
-        read_count+=1
+
+############main#############
+try:
+        while(read_count<100):
+                x=getTemperature()
+                f= open('/var/www/html/nectmatic/simfiles/readings.csv','a')
+                f.write(str(x) + '\n')
+                f.close()
+                print(read_count)
+                read_count+=1
+                time.sleep(sleep_time)
+except (RuntimeError,TypeError,ValueError):
+        print('Value Import Failed')
+        
